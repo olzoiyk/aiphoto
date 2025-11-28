6e53da70ec21d1074399130721427996a6a0fbbc
olzoiyk <olzgre@gmail.com>
2025-11-28 13:27:29 +0100

    added js for clicking images

---
+++ script.js
@@ -0,0 +1,31 @@
+// Get elements
+const galleryImages = document.querySelectorAll('.gallery-grid img');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const closeBtn = document.querySelector('.close');
+
+// Add click event to each image
+galleryImages.forEach(function (img) {
+    img.addEventListener('click', function () {
+
+        //Show the lightbox
+        lightbox.classList.add('active');
+
+        //Set the big image to be the same as the clicked image
+        lightboxImg.src = img.src;
+    });
+});
+
+//Close lightbox
+
+closeBtn.addEventListener('click', function () {
+    //Hide the lightbox
+    lightbox.classList.remove('active');
+});
+
+//Close lightbox when clicking outside the image
+lightbox.addEventListener('click', function (e) {
+    if (e.target === lightbox) {
+        lightbox.classList.remove('active');
+    }
+});
